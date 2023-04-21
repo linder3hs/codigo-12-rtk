cc0d12338d6c749e10dfa60115d88c8d6bd9c6c2
linder3hs <linderhassinger00@gmail.com>
2023-04-20 20:17:19 -0500

    init :fire:

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "./slices/counter";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
---
+++ src/store/slices/counter/index.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      console.log("state", state);
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
